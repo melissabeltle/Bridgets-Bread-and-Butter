7b89782031ed2e4df6c2ca4002a69e0fb573bf43
Melissa Beltle <Melissa.Beltle@waystar.com>
2019-07-18 16:01:54 -0400

    URL update for JS file

--- Bridget's Bread and Butter/bridget.js
+++ Bridget's Bread and Butter/bridget.js
@@ -5,11 +5,11 @@
   var time = 3000;
 
   // IMG
-  images[0] = 'https://github.com/melissabeltle/Bridgets-Bread-and-Butter/blob/master/Bridget's%20Bread%20and%20Butter/img/bread1.jpeg';
-  images[1] = 'https://github.com/melissabeltle/Bridgets-Bread-and-Butter/blob/master/Bridget's%20Bread%20and%20Butter/img/brownie.jpeg';
-  images[2] = 'https://github.com/melissabeltle/Bridgets-Bread-and-Butter/blob/master/Bridget's%20Bread%20and%20Butter/img/cookie3.jpeg';
-  images[3] = 'https://github.com/melissabeltle/Bridgets-Bread-and-Butter/blob/master/Bridget's%20Bread%20and%20Butter/img/muffin2.jpeg';
-  images[4] = 'https://github.com/melissabeltle/Bridgets-Bread-and-Butter/blob/master/Bridget's%20Bread%20and%20Butter/img/cake1.jpeg';
+  images[0] = 'https://user-images.githubusercontent.com/50235978/61487830-cd77ed00-a974-11e9-912d-7c6cbe59df6e.jpeg';
+  images[1] = 'https://user-images.githubusercontent.com/50235978/61487858-da94dc00-a974-11e9-9bea-942e81a988b4.jpeg';
+  images[2] = 'https://user-images.githubusercontent.com/50235978/61487895-ed0f1580-a974-11e9-9901-c59bb77ab6ab.jpeg';
+  images[3] = 'https://user-images.githubusercontent.com/50235978/61487916-fb5d3180-a974-11e9-9842-963a501a994f.jpg';
+  images[4] = 'https://user-images.githubusercontent.com/50235978/61487949-1465e280-a975-11e9-9f79-9a0a2bda60a8.jpeg';
 
   // img change
 
